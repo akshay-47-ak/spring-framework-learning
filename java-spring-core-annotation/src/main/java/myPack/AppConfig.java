6681f4d7747d367f1eccfff690e94fc70bb692e3
akshay-47-ak <akshayathavale1525@gmail.com>
2026-01-09 22:35:56 +0530

    Learn About the Bean And Componet Annotation And Its Diffrance

--- java-spring-core-annotation/src/main/java/myPack/AppConfig.java
+++ java-spring-core-annotation/src/main/java/myPack/AppConfig.java
@@ -9,8 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "looseCoupling")
 public class AppConfig {
+    /* What is Diffrance Between Bean And Component?
 
-    @Bean
+    Both Work same but we use Bean Annotation when we can't make changes in class directly
+    To Avoid changes Direct in Class we use Bean Annotation On the Class Object As Give Below
+     */
+
+    @Bean(initMethod = "init ",destroyMethod = "cleanup")
     public BeanLifeCycle beanLifeCycle(EngineService engineService){
         return  new BeanLifeCycle(engineService);
     }
