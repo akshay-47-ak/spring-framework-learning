3cf9649a0c64612871684ebb4c78df6211665f59
akshay-47-ak <akshayathavale1525@gmail.com>
2026-01-20 20:57:31 +0530

    Learn About the Request Mapping on Class Level And Method Level

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
@@ -11,7 +14,8 @@ public class HelloController {
         return "Hello World";
     }
 
-    @GetMapping("/getUsers")
+   // @GetMapping("/getUsers")
+    @RequestMapping(value = "/getUsers",method = RequestMethod.GET)
     public User getUsers(){
         return new User(1,"Akshay","PADM");
     }
