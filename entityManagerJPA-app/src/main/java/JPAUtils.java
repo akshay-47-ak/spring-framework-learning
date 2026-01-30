87319bfde19de1a8be7965c29d194a0de3f5d1cf
akshay-47-ak <akshayathavale1525@gmail.com>
2026-01-30 21:01:03 +0530

    Learn About the EntityMangerFactory

---
+++ entityManagerJPA-app/src/main/java/JPAUtils.java
@@ -0,0 +1,20 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtils {
+
+    private static final EntityManagerFactory emf;
+
+    static {
+       emf = Persistence.createEntityManagerFactory("TestPersistence");
+    }
+
+    public EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+
+    public static void close(){
+         emf.close();
+    }
+}
