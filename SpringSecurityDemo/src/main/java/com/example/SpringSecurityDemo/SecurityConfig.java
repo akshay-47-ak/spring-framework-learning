7af2809dc8c8ed92ad2c644855837835cc5139d8
akshay-47-ak <akshayathavale1525@gmail.com>
2026-03-16 20:55:20 +0530

    Inmemomry Single user Auth

--- SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/SecurityConfig.java
+++ SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,4 +23,14 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+
 }
