f42b6a50b5699ac3b23244028e632beac73a065d
akshay-47-ak <akshayathavale1525@gmail.com>
2026-03-23 22:22:08 +0530

    Added the Authentication Methiod

--- SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/JWTUtils.java
+++ SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/JWTUtils.java
@@ -9,4 +9,16 @@ public class JWTUtils {
 
     private int jwtExpiresMs=172800000;
 
+    public String getJwtFromHeader(){
+        return "";
+    }
+
+    public String generateTockenFromUsername(){
+        return "";
+    }
+
+    public boolean validateJwtTocken(){
+        return true;
+    }
+
 }
