045923e71318d04e980fc9d22d008f171b2413a8
akshay-47-ak <akshayathavale1525@gmail.com>
2026-03-24 21:05:23 +0530

    Generated the Token

--- SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/JWTUtils.java
+++ SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/JWTUtils.java
@@ -1,6 +1,14 @@
 package com.example.SpringSecurityDemo;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
 
 @Component
 public class JWTUtils {
@@ -13,12 +21,22 @@ public class JWTUtils {
         return "";
     }
 
-    public String generateTockenFromUsername(){
-        return "";
+    public String generateTokenFromUsername(String userName){
+        return Jwts.builder()
+                .subject(userName)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime()+jwtExpiresMs))
+                .signWith(key())
+                .compact()
+                ;
     }
 
-    public boolean validateJwtTocken(){
+    public boolean validateJwtToken(){
         return true;
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
 }
