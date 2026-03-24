9ced06b8ae12b18ffd8fdaf90b2838ecdf224448
akshay-47-ak <akshayathavale1525@gmail.com>
2026-03-24 21:21:16 +0530

    Authetication Logic

--- SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/HelloController.java
+++ SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemo/HelloController.java
@@ -1,11 +1,26 @@
 package com.example.SpringSecurityDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JWTUtils jwtUtils;
 
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/hello")
@@ -21,6 +36,28 @@ public class HelloController {
     @GetMapping("/user/hello")
     public String SayUserHello(){
         return "Hello ,USER";
+    }
+
+    @PostMapping
+    public String login(@RequestBody LoginRequest loginRequest){
+        Authentication authentication = null;
+        try{
+        authentication =authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        }catch (AuthenticationException e){
+            e.printStackTrace();
+            return "Could Not Authenticate";
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+             String jwtToken = jwtUtils.generateTokenFromUsername(userDetails.getUsername());
+
+             return jwtToken;
     }
 
 
